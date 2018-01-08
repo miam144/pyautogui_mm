ef528fc59433e7bf2314342bad3ea1c3d405036a
miam144 <33631606+miam144@users.noreply.github.com>
2018-01-08 10:50:39 -0500

    Add files via upload

---
+++ Mad lib_MM.py
@@ -0,0 +1,63 @@
+import time
+### MAD LIBS ###
+#source http://www.woojr.com/summer-mad-libs/summer-mad-libs-2/
+
+print("Give me a person")
+person1 = input()
+
+print("Give me a place")
+place1 = input()
+
+print("Write an adjective")
+adjective1 = input()
+
+print("Give me the same place")
+place2 = input()
+
+print("Give me a plural noun")
+pluralnoun1 = input()
+
+print("write an adjective")
+adjective2 = input()
+
+print("Write another plural noun")
+pluralnoun2 = input()
+
+print ("Write a place")
+place3 = input()
+
+print("Give me an action verb")
+actionverb1 = input()
+
+print("Think of a plural noun")
+pluralnoun3 = input ()
+
+print("Think of another plural noun")
+pluralnoun4 = input()
+
+print("give me a noun")
+noun1 = input()
+
+print("Write an action verb")
+actionverb2 = input()
+
+print("Write another action verb")
+actionverb3 = input()
+
+print("Think of one last adjective")
+adjective3 = input()
+
+print("Last summer, my family took me and " + person1 + "on a trip to " + place1 + ".")
+print ("The weather there was very " + adjective1 + "!")
+print("Northern " + "has many " + pluralnoun1 + ", and they make" + adjective2 + pluralnoun2 + "there.")
+print("Many people also go to " + place3 + "to " + actionverb1 + "or see the " + pluralnoun3 + ".")
+print("The people that live their love to eat " + pluralnoun4 + " and are very proud of their big" + noun1 +".")
+print("They also like to " + actionverb2 + " in the sun and swim in the " + actionverb3 + "!")
+print("It was a really " + adjective3 + " trip!")
+
+
+time.sleep(100)
+
+
+
+ 
